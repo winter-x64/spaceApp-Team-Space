fd89059bdaf8c36b691baa9635e3915420e937dd
winter64 <akhil.b.xavier3247@gmail.com>
2022-10-02 20:52:22 +0530

    Co-authored-by: Boniface-Noel <Boniface-Noel@users.noreply.github.com> Co-authored-by: Annet Maria Cyriac <Annet-Maria@users.noreply.github.com> Co-authored-by: Athira Rajesh <Athira-03@users.noreply.github.com>

--- editorApp/views.py
+++ editorApp/views.py
@@ -1,18 +1,37 @@
+# Imports
 from django.shortcuts import render
-
-# ? Create your views here.
-# EndPoint for the editor page
-def editor(request):
-    return render(request, 'editorPage.html')
-
-# EndPoint for the preview Page
-def preview(request):
-    return render(request, 'preview.html')
+from editorApp.utils.imageConverter.image_converter import image_convert
 
 # EndPoint For the uploads page
 def upload(request):
-    return render(request, 'uploads.html')
 
-# EndPoint For the uploads page
+    # GET Files from Client Server
+    R_images = request.GET('warmthValue')   #<-  Red image
+    B_images = request.GET('warmthValue')   #<-  Green image
+    G_images = request.GET('warmthValue')   #<-  Blue image
+
+    # Convert the image from grayscale to RGB
+    ConvertImage = image_convert(R_images, G_images, B_images)
+
+    return render(request, 'uploads.html', {'final_image' : ConvertImage})
+
+# EndPoint for the preview Page
+def preview(request):
+
+    return render(request, 'preview.html')
+
+# EndPoint for the editor page
+def editor(request):
+
+    # GET Values from the Client Side
+    brightnessValue = int(request.GET('brightnessValue'))
+    contrastValue   = int(request.GET('contrastValue'))
+    saturationValue = int(request.GET('saturationValue'))
+    warmthValue     = int(request.GET('warmthValue'))
+
+    # render output
+    return render(request, 'editorPage.html')
+
+# EndPoint For the downLoads
 def download(request):
     return render(request, 'uploads.html')
